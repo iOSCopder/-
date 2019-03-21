4ac0b0c77cca2b4e7ed6544eac38153044f1d8d8
errnull <errnull.top@gmail.com>
2019-03-21 17:34:38 +0800

    Initial Commit

---
+++ 阿南中文基础库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  阿南中文基础库
+//
+//  Created by errnull on 2019/3/21.
+//  Copyright © 2019 阿南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
